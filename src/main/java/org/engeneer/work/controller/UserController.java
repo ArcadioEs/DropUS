885ee89541a1dfd00c787fea787dd2e839a82d17
Siuda, Arkadiusz <arkadiusz.siuda@sap.com>
2017-04-06 18:18:24 +0200

    Front-end changes: added deleting funcitonality from the front page.

--- src/main/java/org/engeneer/work/controller/UserController.java
+++ src/main/java/org/engeneer/work/controller/UserController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Controller to display specific {@link UserEntity}.
@@ -31,10 +33,14 @@ public class UserController {
     }
 
     @RequestMapping(value = "add/{username}", method = RequestMethod.GET)
-    public String addUser(@PathVariable("username") String username) {
+    public Map<String, Object> addUser(@PathVariable("username") String username) {
         userService.saveUser(username);
+        final Map<String, Object> model = new HashMap<>();
 
-        return "User " + username + " added to the system.";
+        String message = "User " + username + " added to the system.";
+
+        model.put("message", message);
+        return model;
     }
 
     @RequestMapping(value = "/all", method = RequestMethod.GET)
@@ -43,15 +49,19 @@ public class UserController {
     }
 
     @RequestMapping(value = "/delete/{userId}", method = RequestMethod.GET)
-    public String deleteUser(@PathVariable("userId") Long userId) {
+    public Map<String, Object> deleteUser(@PathVariable("userId") Long userId) {
         final UserEntity user = userService.getUserById(userId);
+        final Map<String, Object> model = new HashMap<>();
+        String message = "User not found or could not be deleted";
 
         if (user != null) {
             final String username = userService.getUserById(userId).getUsername();
             if (userService.deleteUser(userId)) {
-                return "User with id " + userId + " (" + username + ") deleted.";
+                message = "User " + username + " removed from the system";
             }
         }
-        return "User with id " + userId + " could not be found.";
+
+        model.put("message", message);
+        return model;
     }
 }
--- src/main/resources/public/js/UserController.js
+++ src/main/resources/public/js/UserController.js
@@ -3,15 +3,39 @@ var app = angular.module('DropUS', []);
 app.controller('userList', function ($scope, $http) {
 
     $scope.users = [];
+    $scope.addUserMessage = '';
+    $scope.deleteUserMessage = '';
 
     $scope.getAllUsers = function() {
         $http({
             method: 'GET',
             url: 'user/all'
-        }).then(function(obj) {
-            $scope.users = obj.data;
-            console.log(obj)
+        }).then(function(model) {
+            $scope.users = model.data;
         });
-    }
+    };
 
+    $scope.addUser = function() {
+        $http({
+            method: 'GET',
+            url: 'user/add/' + $scope.usernameToAdd
+        }).then(function (model) {
+            $scope.addUserMessage = model.data.message;
+            $scope.getAllUsers();
+        });
+    };
+
+    $scope.deleteUser = function() {
+        if( !isNaN($scope.userIdToDelete) && angular.isNumber(+$scope.userIdToDelete)) {
+            $http({
+                method: 'GET',
+                url: 'user/delete/' + $scope.userIdToDelete
+            }).then(function (model) {
+                $scope.deleteUserMessage = model.data.message;
+                $scope.getAllUsers();
+            });
+        } else {
+            $scope.deleteUserMessage = 'Please provide user id, not a name';
+        }
+    };
 });
